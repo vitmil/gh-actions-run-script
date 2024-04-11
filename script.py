cc49a047863ab5ee8039d65cd047068f5e55fb64
vimi <vimi@controller-ub.vbox.local>
2024-04-11 11:59:56 +0200

    vimi commit

--- script.py
+++ script.py
@@ -1,10 +1,22 @@
-import sys
-## TODO: installare requests nelle actions...
 import requests
-response = requests.get('http://ident.me')
-print()
-print(f"response test: {response.text}")
-print()
-#print('Hello DevOps')
-#print()
-#print(sys.version)
+
+def check_url_request(url):
+    """ funzione request """
+    try:
+        response = requests.get(url, timeout=3)
+        return True, response.text
+    except Exception as e:
+        print(f"\n[ERRORE]: {e}")
+        return False, "altro che non è reposne.text perchè se non ha fatto la response non può esistere"
+
+if __name__ == "__main__":
+
+    URL = "http://ident.me"
+    r = check_url_request(URL)
+    if r[0]:
+        response_text = r[1]
+        print(f"Ip Address: {response_text}")
+    else:
+        print("\nrequest fallita\n")
+    
+    
