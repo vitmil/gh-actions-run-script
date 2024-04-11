68a037bb2f2a8a01b94567b815685bc2fce5cd8a
vimi <vimi@controller-ub.vbox.local>
2024-04-11 13:05:25 +0200

    vimi commit

--- unit-test/unit_test.py
+++ unit-test/unit_test.py
@@ -1,4 +1,13 @@
+import os
+
 print('\nHello Unit Test!\n')
+
+ci_cd_outcome = os.getenv('CI_CD_OUTCOME')
+if ci_cd_outcome == "OK":
+    print("[OK]: Il valore della variabile settata dal workflow-action è == a OK")
+else:
+    print("[OK]: Il valore della variabile settata dal workflow-action è == a OK")
+    exit(1)
 
 
 
