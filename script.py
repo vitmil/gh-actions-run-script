ffe03690cad1b29a982e8314d08e85a883b597c1
Vittorio Milazzo <38243931+vitmil@users.noreply.github.com>
2024-04-11 09:41:30 +0200

    Update script.py

--- script.py
+++ script.py
@@ -2,13 +2,13 @@
 questo commento è necessario per la action pylint per ebitare errore
 script.py:1:0: C0114: Missing module docstring (missing-module-docstring)
 """
-#import os
-#import sys
+import sys
+## todo: installare requests per le GH actions
 #import requests
-
 
 #response = requests.get('http://ident.me')
 #print(f"response test: {response.text}")
 print()
 print('Hello DevOps')
 print()
+print(os.version)
