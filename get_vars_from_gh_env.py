9647e1cdcf948032071830ef2b2a3ed93d00fc23
vimi <vimi@controller-ub.vbox.local>
2024-04-18 13:07:55 +0200

    my commit

--- get_vars_from_gh_env.py
+++ get_vars_from_gh_env.py
@@ -1,1 +1,7 @@
-print(MY_VAR_1)
+import sys
+
+# Recupera il primo argomento dalla lista degli argomenti passati allo script
+primo_argomento = sys.argv[1]
+
+# Stampa il primo argomento
+print(f"Primo argomento: {primo_argomento}")
